768467f211008adced77b7360e896e29766ce9d3
cocoffee <cocoffee724@gmail.com>
2022-03-02 23:40:48 +0900

    [new] main_import_data.js / js에서 분리된 html 문서 불러오기

---
+++ js/src/main_import_data.js
@@ -0,0 +1,55 @@
+//main_import_data.js
+
+(function($){
+
+  //공통영역 불러오기=============================
+  //변수
+  const body = $('body');
+  const headBox = $('#headBox');
+  const footBox = $('#footBox');
+  const baseUrl = "../page/common/";
+  let importPage = ['headBox.html','footBox.html'];
+
+  //기능수행
+  headBox.load(baseUrl+importPage[0],function(){
+    body.append('<script src="../js/src/headBox_modal.js"></script><script src="../js/src/headBox_menu_tab.js"></script>');
+  });
+  // js: 모달이랑 탭기능이랑 합칠지 고민
+  footBox.load(baseUrl+importPage[1]);
+
+
+
+
+  //메인영역 불러오기===========================
+  //변수
+  const slideBox = $('#slideBox');
+  const bestBox = $('#bestBox');
+  const saleBox = $('#saleBox');
+  const newBox = $('#newBox');
+  const blogBox = $('#blogBox');
+  const mainUrl ="../page/main/";
+  let importMain = ['slideBox.html','bestBox.html','saleBox.html','newBox.html','blogBox.html'];
+  let mainSelect = [slideBox,    bestBox,    saleBox,    newBox,    blogBox ];
+  
+  let importLen = importMain.length;
+    //기능수행
+  // slideBox.load(mainUrl+importMain[0]);
+  // bestBox.load(mainUrl+importMain[1]);
+  // saleBox.load(mainUrl+importMain[2]);
+  // newBox.load(mainUrl+importMain[3]);
+  // blogBox.load(mainUrl+importMain[4]);
+
+  // let i = 0;
+  // for ( ; i < importLen ; i+=1 ){
+  //   mainSelect[i].load(mainUrl+importMain[i]);
+  // }
+
+  $.each(mainSelect,function(idx,selector){
+    selector.load(mainUrl+importMain[idx],function(){
+      if( idx === 0){
+        body.append('<script src="../js/src/slideBox.js" class="pc_slide"></script>');
+      }
+    });
+  });
+
+})(jQuery);
