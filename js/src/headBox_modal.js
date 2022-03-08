b8ce74361f192332f057e64febabe0a38168d7d0
cocoffee <cocoffee724@gmail.com>
2022-03-08 21:28:47 +0900

    [update] headBox-modal.js / unb 호버 스타일 추가 및 모달 닫기 스타일 추가

--- js/src/headBox_modal.js
+++ js/src/headBox_modal.js
@@ -7,21 +7,43 @@
 
 
 //변수
-const navBox = $('#navBox');
-const unb = navBox.children('.unb');
-const modalWindow = $('.modal_window');
+var navBox = $('#navBox');
+var unb = navBox.children('.unb');
+var modalWindow = $('.modal_window');
 
 // unb안에 ul,li만들기
-const mkUnb =  '<ul class="clearfix"><li><a href="./login.html"><p>로그인</p></a></li><li><a href="./join_page.html"><p>회원가입</p></a></li><li><button type="button"><p>한국어</p></button></li></ul>'
+var mkUnb =  '<ul class="clearfix"><li><a href="./login.html"><p>로그인</p></a></li><li><a href="./join_page.html"><p>회원가입</p></a></li><li><button type="button"><p>한국어</p></button></li></ul>'
 unb.append(mkUnb);
 
 // 모달 만들기
-const mkModal = '<div class="modal_data"><div class="close_btn"><button type="button">  <span class="blind">닫기</span><i class="fa-solid fa-xmark"></i></button></div><div class="modal_content"><h2>국가/언어/통화 선택</h2><span class="content1"><p></p></span></div></div><div class="modal_bg"></div>';
+var mkModal = '<div class="modal_data"><div class="close_btn"><button type="button">  <span class="blind">닫기</span><i class="fa-solid fa-xmark"></i></button></div><div class="modal_content"><h2>국가/언어/통화 선택</h2><span class="content1"><p></p></span></div></div><div class="modal_bg"></div>';
 modalWindow.html(mkModal);
 
-let languageBtn = unb.find('button');
-let modalCloseBtn = modalWindow.find('button');
-let modalBg = modalWindow.find('.modal_bg');
+var languageBtn = unb.find('button');
+var modalCloseBtn = modalWindow.find('button');
+var closeIcon = modalWindow.find('.fa-xmark');
+var modalBg = modalWindow.find('.modal_bg');
+
+var loginBtn = unb.find('li').eq(0);
+var joinBtn = unb.find('li').eq(1);
+
+// 로그인,회원가입 영역에 mouseenter시 글씨크기 변경/되돌리기
+loginBtn.on('mouseenter',function(e){
+  e.preventDefault();
+  loginBtn.css({  fontWeight : 700 });
+});
+loginBtn.on('mouseleave',function(e){
+  e.preventDefault();
+  loginBtn.removeAttr('style');
+});
+joinBtn.on('mouseenter',function(e){
+  e.preventDefault();
+  joinBtn.css({  fontWeight : 700 });
+});
+joinBtn.on('mouseleave',function(e){
+  e.preventDefault();
+  joinBtn.removeAttr('style');
+});
 
 
 languageBtn.on('click',function(e){
@@ -32,6 +54,16 @@ modalCloseBtn.on('click',function(e){
   e.preventDefault();
   modalWindow.stop().fadeOut();
 });
+
+modalCloseBtn.on('mouseenter',function(e){
+  e.preventDefault();
+  closeIcon.css({ transform: 'scale('+2+')'});
+});
+modalCloseBtn.on('mouseleave',function(e){
+  e.preventDefault();
+  closeIcon.removeAttr('style');
+});
+
 
 modalBg.on('click',function(e){
   e.preventDefault();
