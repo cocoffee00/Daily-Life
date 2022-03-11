4007c74095cd085432e87ff2cf73e1d29da72159
cocoffee <cocoffee724@gmail.com>
2022-03-11 22:18:01 +0900

    [update] blogBox.js / 갤러리 이미지 적용중

--- js/src/blogBox.js
+++ js/src/blogBox.js
@@ -28,9 +28,13 @@
 
   // 갤러리 타이틀 구역 생성
   var titlePart = blogArea.find('.blog_title_part');
-  var titleInnerSet = ' <div class="blog_title"><h3>blog title</h3></div><span><p>Lorem, ipsum dolor sit amet consectetur adipisicing.</p></span><ul class="clearfix"></ul></div>'
+  var titleInnerSet = ' <div class="blog_title"><h3>Daily Blog</h3></div><span><p>데일리 라이프 블로그에 방문하여 다양한 정보를 확인해보세요.</p></span><ul class="clearfix"></ul></div>'
   titlePart.html(titleInnerSet);
 
+
+
+
+  //===========================================다시 수정하기=====================
   // 갤러리 버튼 구역 생성
   
   var test = function(tit,img){
@@ -44,36 +48,55 @@
     
     var btnImg = titleUl.find('.contant_thumbnail');
     btnImg.css({backgroundImage : 'url('+url+img+')'});
-    // console.log(url+img);
+    console.log(url+img);
 
   }
 
   var i = 0;
   for( ; i<dataLen; i+=1){
     test(data[i].title,data[i].img);
+    // console.log(data[i].img);
   };
-    
   
-  
-  
-  var titleLi = titlePart.children('li');
-  var titleBtn = titleLi.children('button');
-  var tumbnail = titleLi.find('.contant_thumbnail');
 
-  //이벤트
-  titleBtn.on('click',function(e){
-    e.preventDefault();
-    var clickBtn = $(this).parent().index();
-    console.log(clickBtn);
-  })
+  
+  // var test = function(tit,img){
+
+  //   var btnImg= btnImg.css({backgroundImage : 'url('+url+img+')'});
+    
+  //   var titleUl = titlePart.find('ul');
+  //   var titleLiSet = '<li><img src="'+btnImg+'" alt=""><span>'+tit+'</span></li>';
+  //   titleUl.append(titleLiSet);
+
+  // };
+
+  // var test2 = function(n){
+
+  // }
+
 
   
 
   // 갤러리 이미지 구역 생성
   var blogContent = blogArea.find('.blog_content');
 
-  var blogConSet = '<div class="blog_content"><a href="#"><div class="blog_gallery"><span class="blind"></span></div></a></div>';
+  var blogConSet = '<div class="blog_content"><a href="#"><div class="blog_gallery on"><span class="blind"></span></div></a></div>';
   blogContent.html(blogConSet);
+  
+  var titleLi = titlePart.children('li');
+  var titleBtn = titleLi.children('button');
+
+
+
+
+  //이벤트
+  // titleBtn.on('click',function(e){
+  //   e.preventDefault();
+  //   var clickBtn = $(this).parent().index();
+  //   // console.log(clickBtn);
+  // })
+
+  
 
 
 
