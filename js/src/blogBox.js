b93ec38346ebc5bce20580eec933c55be20cfb5d
cocoffee <cocoffee724@gmail.com>
2022-03-14 18:08:14 +0900

    [update] blogBox.js / 구현 완 + 추후 테스트용 더미 데이터 분리 및 추가 필

--- js/src/blogBox.js
+++ js/src/blogBox.js
@@ -4,12 +4,12 @@
 
   // 테스트용 더미 데이터 ==========================================
   var jsonData = [
-    {'title' : '건강한 하루를 만드는 습관 5',
-      'img' : 'blog_01_thumb.png'},
-    {'title' : '비타민의 7가지 효능',
-    'img' : 'blog_02_thumb.png'},
+    {'title' : '건강한 하루를 만드는 습관 5가지',
+      'image' : 'blog_01_thumb.png'},
     {'title' : '하루 10분의 러닝이 생활을 바꾼다.',
-    'img' : 'blog_03_thumb.png'}
+    'image' : 'blog_02_thumb.png'},
+    {'title' : '비타민의 7가지 효능',
+    'image' : 'blog_03_thumb.png'}
   ];
   //================================================================
 
@@ -18,6 +18,8 @@
   var blogArea = blogBox.children('.blog_area');
   var data = jsonData;
   var dataLen = data.length;
+  var url = '../img/blog/';
+  
   
 
   // 기본 레이아웃 구역 생성
@@ -32,71 +34,67 @@
   titlePart.html(titleInnerSet);
 
 
+   // 갤러리 이미지 구역 생성
+  
+   var blogContent = blogArea.find('.blog_content');
+   var blogConSet = '<div><div class="blog_gallery"><div class="img_wrap"><span></span><div class="more"><a href="#"><spna>자세히보기</span></a></div></div></div></div>';
+   blogContent.append(blogConSet);
+   
+   var blogDiv = blogContent.children('div');
+   var divImg = blogDiv.find('.blog_gallery');
+   var moreBtn = divImg.find('.more');
+   var moreA = moreBtn.find('a');
 
+   // 메인 컨텐츠 내용 구성하기
+   var mainBlogFn =function(a){
+    var blogTitle = blogDiv.find('span');
+    var dataN = data[a]
+    
+    divImg.css({backgroundImage : 'url('+url+dataN.image+')'});
+    blogTitle.text(dataN.title)
+   }
+   mainBlogFn(0);
 
-  //===========================================다시 수정하기=====================
+  // 메인 컨텐츠 마우스 오버 적용
+   moreBtn.on('mouseover',function(){
+    moreA.css({borderRadius : '10px' ,backgroundColor: 'rgba(100, 100, 100, 0.5)' });
+   })
+   moreBtn.on('mouseleave',function(){
+    moreA.removeAttr('style');
+   })
+  
+
   // 갤러리 버튼 구역 생성
-  
-  var test = function(tit,img){
-    
-    var titleUl = titlePart.find('ul');
-    var titleLiSet = '<li><button type="button"><span class="blind"></span><div class="contant_thumbnail"></div><span>'+tit+'</span></button></li>';
-    titleUl.append(titleLiSet);
-
-    
-    var url = '../img/blog/';
-    
-    var btnImg = titleUl.find('.contant_thumbnail');
-    btnImg.css({backgroundImage : 'url('+url+img+')'});
-    console.log(url+img);
-
-  }
-
+  var titleUl = titlePart.find('ul');
+  var titleLiSet = '<li><button type="button"><div class="contant_thumbnail"></div><span></span></button></li>';
   var i = 0;
-  for( ; i<dataLen; i+=1){
-    test(data[i].title,data[i].img);
-    // console.log(data[i].img);
-  };
   
-
-  
-  // var test = function(tit,img){
-
-  //   var btnImg= btnImg.css({backgroundImage : 'url('+url+img+')'});
+  // 각 버튼마다 제이슨에서 불러운 이미지 및 타이틀 담기
+  var subTitleFn = function(n){
+    titleUl.append(titleLiSet);
     
-  //   var titleUl = titlePart.find('ul');
-  //   var titleLiSet = '<li><img src="'+btnImg+'" alt=""><span>'+tit+'</span></li>';
-  //   titleUl.append(titleLiSet);
-
-  // };
-
-  // var test2 = function(n){
-
-  // }
-
-
+    var dataN = data[n];
+    var btnLiN = titleUl.find('li').eq(n);
+    var btnTitle = btnLiN.find('span');
+    var btnImg = btnLiN.find('.contant_thumbnail');
+    
+    btnTitle.text(dataN.title)
+    btnImg.css({backgroundImage : 'url('+url+dataN.image+')'});
+  }
+  
+  for ( ; i < dataLen ; i+=1 ){
+    subTitleFn(i);
+  }
   
 
-  // 갤러리 이미지 구역 생성
-  var blogContent = blogArea.find('.blog_content');
-
-  var blogConSet = '<div class="blog_content"><a href="#"><div class="blog_gallery on"><span class="blind"></span></div></a></div>';
-  blogContent.html(blogConSet);
+  // 버튼 클릭시 이벤트
+  var titleBtn = titleUl.find('button');
   
-  var titleLi = titlePart.children('li');
-  var titleBtn = titleLi.children('button');
-
-
-
-
-  //이벤트
-  // titleBtn.on('click',function(e){
-  //   e.preventDefault();
-  //   var clickBtn = $(this).parent().index();
-  //   // console.log(clickBtn);
-  // })
-
-  
+  titleBtn.on('click',function(e){
+    e.preventDefault();
+    var i = $(this).parent().index();
+    mainBlogFn(i)
+  })
 
 
 
